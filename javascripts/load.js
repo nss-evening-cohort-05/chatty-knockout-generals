15023bacaf7d9c4ec009ad2b04d78e2671bb5fdf
Matt Hensley <matt.hensley777@gmail.com>
2017-03-14 19:00:32 -0500

    first load commit

---
+++ javascripts/load.js
@@ -0,0 +1,49 @@
+
+
+var ClipChat = (function(oldClipChat){
+
+		oldClipChat.testLoad = function(){
+			console.log("load is working");
+		}
+
+		return oldClipChat;
+
+})(ClipChat || {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
