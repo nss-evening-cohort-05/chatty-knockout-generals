1be2d90bf47056579a084bf9e045adc19211d941
Matt Hensley <matt.hensley777@gmail.com>
2017-03-14 21:01:21 -0500

    commit

--- javascripts/load.js
+++ javascripts/load.js
@@ -1,36 +1,24 @@
 var ClipChat = (function(oldClipChat) {
 
-    var chatContainer = document.getElementById(""); {
 
-        function makeDom(data) {
-            var chatString = "";
-            var currentChat;
-            for (var i = 0; i < data.chat.length; i++);
-            currentChat = data.chat[i];
+	function loadFile() {
 
-            chatString += `<div>`;
-            chatString += `<h4>${currentChat.text}</h4>`;
-            chatString += `<img src="${}`;
-
-            chatContainer.innerHTML = chatString;
-
-        }
+        var data = JSON.parse(this.responseText);
+        makeDom(data);
     }
 
+    function loadIfFileFails() {
 
 
+    }
 
-
-
-
-
+    var myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", loadFile);
+    myRequest.addEventListener("error", loadIfFileFails);
+    myRequest.open("GET", "messages.json");
+    myRequest.send();
 
     return oldClipChat;
 
-    myRequest.open("GET", "messages.json");
-
 
 })(ClipChat || {});
-
-
-
