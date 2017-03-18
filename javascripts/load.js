4e6937c0f174bcd4e9dfca69e926347cf1d76860
Ben Greaves <bsgreaves@gmail.com>
2017-03-18 12:09:54 -0500

    Removed console.logs

--- javascripts/load.js
+++ javascripts/load.js
@@ -6,13 +6,9 @@ var ClipChat = (function(oldClipChat) {
         console.log("json error");
     },
     oldClipChat.loadJSON = function(filepath) {
-    	console.log("this", this);
     	let data = JSON.parse(this.responseText);
-        console.log("data1", data);
     	data = data.messages;
-        console.log("data2", data);
     	jsonArray.push(data);
-    	console.log("jsonArray", jsonArray);
         if (jsonArray.length === 5) {
         	jsonArray = jsonArray.reduce(function(final, each) {
         		return final.concat(each);
@@ -34,19 +30,5 @@ var ClipChat = (function(oldClipChat) {
 
 var jsonPaths = ["javascripts/msg1.json", "javascripts/msg2.json", "javascripts/msg3.json", "javascripts/msg4.json", "javascripts/msg5.json"];
 jsonPaths.forEach(i => ClipChat.xhrRequest(i));
-
-// var myRequest = new XMLHttpRequest();
-// myRequest.addEventListener("load", ClipChat.loadFile);
-// myRequest.addEventListener("error", ClipChat.jsonError);
-// myRequest.open("GET", "javascripts/messages.json");
-// myRequest.send();
-
-// function xhrRequest(filePath){
-// 	let xhr = new XMLHttpRequest();
-// 	xhr.addEventListener("load", ClipChat.loadJSON);
-// 	xhr.addEventListener("error", ClipChat.jsonError);
-// 	xhr.open("GET", filePath);
-// 	xhr.send();
-// }
 
 
